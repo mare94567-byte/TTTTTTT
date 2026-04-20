1455f54395c2a476883b9d28e3146428f65b4bce
mare94567-byte <mare94567@gmail.com>
2026-04-20 10:46:12 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,7 @@ class _SamaqAppState extends State<SamaqApp> {
         primarySwatch: Colors.blue,
         brightness: isDarkMode ? Brightness.dark : Brightness.light,
         scaffoldBackgroundColor: isDarkMode ? const Color(0xFF0F0F0F) : Colors.white,
+        appBarTheme: const AppBarTheme(backgroundColor: Colors.blueAccent),
       ),
       home: HomeScreen(onThemeChanged: toggleTheme, isDarkMode: isDarkMode),
     );
